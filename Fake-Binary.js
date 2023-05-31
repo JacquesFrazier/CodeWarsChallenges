e5ddc4b49a82d4247fd4da9306583f4c1f1a617b
JacquesFrazier <129914599+JacquesFrazier@users.noreply.github.com>
2023-05-31 19:05:46 -0400

    Delete Fake-Binary.js

--- Fake-Binary.js
+++
@@ -1,8 +0,0 @@
-//question
-// Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
-
-
-//solution 
-function fakeBin(x) {
-    return x.split('').map(n => n < 5 ? 0 : 1).join('');
-}
