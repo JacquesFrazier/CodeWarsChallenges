c6ea9787df1210c6701b93bcb1e64bacaa3d70f5
JacquesFrazier <129914599+JacquesFrazier@users.noreply.github.com>
2023-06-08 15:06:19 -0400

    Add files via upload

---
+++ 8kyu/reversed-strings.js
@@ -0,0 +1,12 @@
+//question 
+
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+//soultion
+
+function solution(str){
+  return [...str].reverse().join('');
+}
