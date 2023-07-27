8b967950686acb36976bc9f7a3510ba23e375f95
JacquesFrazier <129914599+JacquesFrazier@users.noreply.github.com>
2023-07-27 16:52:21 -0400

    Add files via upload

---
+++ 8kyu/Convert-a-Boolean-to-a-String.js
@@ -0,0 +1,17 @@
+//problem 
+
+//Implement a function which convert the given boolean value into its string representation.
+
+//Note: Only valid inputs will be given.
+
+//answer 1
+
+function booleanToString(b){
+    return b ? 'true' : 'false';
+  }
+
+//answer 2
+
+function booleanToString(b){
+    return b.toString();
+  }
