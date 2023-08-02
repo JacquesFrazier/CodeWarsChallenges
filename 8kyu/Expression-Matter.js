9a343d2dc2837b85ffc3905baf61242310459bfa
JacquesFrazier <129914599+JacquesFrazier@users.noreply.github.com>
2023-08-02 12:58:00 -0400

    Update Expression-Matter.js

--- 8kyu/Expression-Matter.js
+++ 8kyu/Expression-Matter.js
@@ -30,3 +30,54 @@ function expressionMatter(a, b, c) {
 function expressionMatter(a, b, c) {
     return Math.max(a+b+c, a*b*c, (a+b)*c, a*(b+c));
   }
+
+//answer 3
+
+function expressionMatter(a, b, c) {
+  
+  if(a==1 && b==1 && c==1){
+    return 3;
+  }
+  
+  if(a!=1 && b!=1 && c!=1){
+    return a*b*c;
+  }
+  if(a==1 && b!=1 && c!=1){
+    return (a+b)*c
+  }
+   if(a!=1 && b!=1 && c==1){
+    return a*(b+c);
+  }
+ 
+  if(a!=1 && b==1 && c==1){
+    return a*2;
+  }
+ 
+  if(a==1 && b==1 && c!=1){
+    return c*2;
+  }
+ 
+  if(a==1 && b!=1 && c==1){
+    if(a==c){
+      return a+b+c;
+    }
+    if(a>c){
+      return a*(b+c);
+    }
+    if(a<c){
+      return (a+b)*c;
+    }
+  }
+  
+  if(a!=1 && b==1 && c!=1){
+    if(a==c){
+      return (a+b)*c;
+    }
+    if(a>c){
+      return a*(b+c);
+    }
+    if(a<c){
+      return (a+b)*c;
+    } 
+  } 
+}
