f3be46be0bc07db41a73504666d19d52cb779dc5
JacquesFrazier <129914599+JacquesFrazier@users.noreply.github.com>
2023-08-03 11:01:27 -0400

    Add files via upload

---
+++ 7kyu/Count-the-Digit.js
@@ -0,0 +1,45 @@
+//problem
+
+//Take an integer n (n >= 0) and a digit d (0 <= d <= 9) as an integer.
+
+//Square all numbers k (0 <= k <= n) between 0 and n.
+
+//Count the numbers of digits d used in the writing of all the k**2.
+
+//Implement the function taking n and d as parameters and returning this count.
+
+//Examples:
+//n = 10, d = 1 
+//the k*k are 0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100
+//We are using the digit 1 in: 1, 16, 81, 100. The total count is then 4.
+
+//The function, when given n = 25 and d = 1 as argument, should return 11 since
+//the k*k that contain the digit 1 are:
+//1, 16, 81, 100, 121, 144, 169, 196, 361, 441.
+//So there are 11 digits 1 for the squares of numbers between 0 and 25.
+//Note that 121 has twice the digit 1.
+
+//answer 1
+
+function nbDig(n, d) {
+    var res=0;
+        for (var g=0;g<=n;g++){
+          var square=(g*g+"").split("");
+          square.forEach((s)=>s==d?res++:null)
+        }return res;
+    }
+
+//answer 2
+
+function nbDig(n, d) {
+    var o = '';
+      for(var i = 0; i <= n; i++){
+        o += Math.pow(i, 2);
+      }
+    return o.split(d).length-1
+  }
+
+  //answer 3
+
+  nbDig = (n, d) => Array.from(Array(n + 1), (e, i) => i * i).join('').match(new RegExp(d, 'g')).length;
+
