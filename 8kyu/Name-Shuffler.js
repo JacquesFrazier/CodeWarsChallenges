c878e1425b31c778f4e71be15228d582ed40dc5e
JacquesFrazier <129914599+JacquesFrazier@users.noreply.github.com>
2023-08-08 16:17:19 -0400

    Add files via upload

---
+++ 8kyu/Name-Shuffler.js
@@ -0,0 +1,13 @@
+//problem 
+
+//Write a function that returns a string in which firstname is swapped with last name.
+
+//Example(Input --> Output)
+
+"john McClane" --> "McClane john"
+
+//answer 
+
+function nameSuffle(str){
+    return str.split(' ').reverse().join(' ')
+  }
