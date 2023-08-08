f2fb35c19c341ca4c06ecb6426902cb2a273eb08
JacquesFrazier <129914599+JacquesFrazier@users.noreply.github.com>
2023-08-08 16:25:09 -0400

    Add files via upload

---
+++ 8kyu/Training-JS-#1-create-your-first-JS-function-and-print-'HelloWorld!'.js
@@ -0,0 +1,33 @@
+//problem 
+
+//Please refer to two example above and write your first JS function.
+
+//mission 1:
+
+//use keyword function to define your function, function name should be helloWorld(don't forget the () and {})
+
+//mission 2:
+
+//use keyword var (or let or const) to define a variable str, value of str should be a string: "Hello World!"(don't forget the =).
+
+//mission 3:
+
+//type the console.log() in the next line (don't forget to put the str in the parentheses).
+
+//When you have finished the work, click "Run Tests" to see if your code is working properly.
+
+//In the end, click "Submit" to submit your code pass this kata.
+
+//Series
+//( ↑↑↑ Click the link above can get my newest kata list, Please add it to your favorites)
+
+//#1: create your first JS function helloWorld
+//#2: Basic data types--Number
+//#3: Basic data types--String
+
+//answer 
+
+function helloWorld() {
+    let str = 'Hello World!'
+    console.log(str)
+  }
