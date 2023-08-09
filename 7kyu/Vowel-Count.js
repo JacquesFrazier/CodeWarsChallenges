e6f4056c0f52ed27dd98b5f653384cc89151eb51
JacquesFrazier <129914599+JacquesFrazier@users.noreply.github.com>
2023-08-09 15:25:54 -0400

    Update Vowel-Count.js

--- 7kyu/Vowel-Count.js
+++ 7kyu/Vowel-Count.js
@@ -18,3 +18,31 @@ function getCount(str) {
 function getCount(str) {
   return (str.match(/[aeiou]/ig)||[]).length;
 }
+
+//answer 3
+
+function getCount(str) {
+  var vowelsCount = 0;
+  var vowels = ["a","e","i","o","u"];
+  for(var i = 0;i < str.length;i++){
+    for(var j=0;j<vowels.length;j++){
+      if(str[i] === vowels[j]){
+        vowelsCount++;
+      }
+    }
+  }
+  
+  return vowelsCount;
+}
+
+//answer 4
+
+function getCount(str) {
+ return str.split('').filter(c => "aeiouAEIOU".includes(c)).length;
+}
+
+//answer 5
+
+function getCount(str) {
+  return str.replace(/[^aeiou]/gi, '').length;
+}
