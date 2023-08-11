2f5030bd0f77bbfd14f04875c6168284a80f46b6
JacquesFrazier <129914599+JacquesFrazier@users.noreply.github.com>
2023-08-11 17:27:57 -0400

    Add files via upload

---
+++ 8kyu/Total-amount-of-points.js
@@ -0,0 +1,44 @@
+//problem 
+
+//Our football team has finished the championship.
+
+//Our team's match results are recorded in a collection of strings. Each match is represented by a string in the format "x:y", where x is our team's score and y is our opponents score.
+
+//For example: ["3:1", "2:2", "0:1", ...]
+
+//Points are awarded for each match as follows:
+
+//if x > y: 3 points (win)
+//if x < y: 0 points (loss)
+//if x = y: 1 point (tie)
+//We need to write a function that takes this collection and returns the number of points our team (x) got in the championship by the rules given above.
+
+Notes:
+
+//our team always plays 10 matches in the championship
+0 <= x <= 4
+0 <= y <= 4
+
+//answer 1
+
+const points=games=>games.reduce((output,current)=>{
+    return output += current[0]>current[2] ? 3 : current[0]===current[2] ? 1 : 0;
+  },0)
+
+//answer 2
+
+function points(games) {
+    let total = 0;
+    games.map(game => {
+      if (game[0] === game[2]) {
+        total += 1;
+      } else if (game[0] > game[2]) {
+        total += 3;
+      }
+    });
+    return total;
+  }
+
+//answer 3
+
+const points = g => g.reduce((a, [x, _, y]) => a + (x > y ? 3 : x == y), 0)
