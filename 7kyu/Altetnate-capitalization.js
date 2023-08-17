1f05fc8afd3e8c16aa2e5830cbe13d7000827e72
JacquesFrazier <129914599+JacquesFrazier@users.noreply.github.com>
2023-08-17 12:36:29 -0400

    Update Altetnate-capitalization.js

--- 7kyu/Altetnate-capitalization.js
+++ 7kyu/Altetnate-capitalization.js
@@ -18,8 +18,10 @@ function capitalize(s){
   return [even, odd];
 };
 
-
 //answer 2
 
+function capitalize(s){
+  return [0,1].map(r=>[...s].map((c,i)=>i%2===r?c.toUpperCase():c).join(''));
+};
 
 //answer 3
