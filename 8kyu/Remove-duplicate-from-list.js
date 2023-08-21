78036bf430788f7befa621025afafe5cb4a73dc8
JacquesFrazier <129914599+JacquesFrazier@users.noreply.github.com>
2023-08-21 17:19:22 -0400

    Update Remove-duplicate-from-list.js

--- 8kyu/Remove-duplicate-from-list.js
+++ 8kyu/Remove-duplicate-from-list.js
@@ -1,10 +1,10 @@
 //problem 
 
-Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
+//Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
 
-The order of the sequence has to stay the same.
+//The order of the sequence has to stay the same.
 
-Examples:
+//Examples:
 
 Input -> Output
 [1, 1, 2] -> [1, 2]
