2a2fbc966210dce587f6dfbcb9fc274fdc49d489
JacquesFrazier <129914599+JacquesFrazier@users.noreply.github.com>
2023-08-23 11:05:43 -0400

    Update Sum-of-a-sequence.js

--- 7kyu/Sum-of-a-sequence.js
+++ 7kyu/Sum-of-a-sequence.js
@@ -34,3 +34,13 @@ const sequenceSum = (begin, end, step) => {
 };
 
 //answer 3
+
+function sequenceSum(begin, end, step) {
+  let sum = 0
+  for (let i = begin; i <= end; i += step) {
+    sum += i
+  }
+  return sum
+}
+
+//
