b32a482611d2b0f18a9098f9ddab1f672aa15a99
JacquesFrazier <129914599+JacquesFrazier@users.noreply.github.com>
2023-08-27 16:35:40 -0400

    Update Replace-With-Alphabet-Position.js

--- 6kyu/Replace-With-Alphabet-Position.js
+++ 6kyu/Replace-With-Alphabet-Position.js
@@ -30,3 +30,13 @@ let alphabetPosition = (text) => text.toUpperCase().replace(/[^A-Z]/g, '').split
 
 //answer 3
 
+function alphabetPosition(text) {
+  var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  return text.toLowerCase().split('')
+  .filter( letter => {
+    let index = alphabet.indexOf(letter);
+    return index > -1;
+  }  )
+  .map( letter => alphabet.indexOf(letter) + 1 )
+  .join(' ')
+}
