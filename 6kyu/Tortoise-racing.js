8139fe49429321739517cad0dba2d42de8147d0b
JacquesFrazier <129914599+JacquesFrazier@users.noreply.github.com>
2023-08-28 14:02:53 -0400

    Update Tortoise-racing.js

--- 6kyu/Tortoise-racing.js
+++ 6kyu/Tortoise-racing.js
@@ -73,5 +73,17 @@ function race(v1, v2, g) {
 
 //answer 4
 
+function race (v1, v2, g) {
+
+  if (v1 >= v2) return null
+
+  const time = g / ( v2 - v1 )
+
+  let hours = Math.floor(time)
+  let minutes = Math.floor((time * 60) % 60)
+  let seconds = Math.floor((time * 3600) % 60)
+
+  return [ hours, minutes, seconds ]
+}
 
 //answer 5
