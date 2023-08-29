80cfb67901cd0adfef786431854d47e914f083ff
JacquesFrazier <129914599+JacquesFrazier@users.noreply.github.com>
2023-08-29 18:13:27 -0400

    Update Build-Tower.js

--- 6kyu/Build-Tower.js
+++ 6kyu/Build-Tower.js
@@ -33,6 +33,14 @@ function towerBuilder(n) {
 
 //answer 2
 
-
+function towerBuilder(nFloors) {
+  var tower = [];
+  for (var i = 0; i < nFloors; i++) {
+    tower.push(" ".repeat(nFloors - i - 1)
+             + "*".repeat((i * 2)+ 1)
+             + " ".repeat(nFloors - i - 1));
+  }
+  return tower;
+}
 
 //answer 3
