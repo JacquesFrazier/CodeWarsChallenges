e7931f41469c17d3e6df90f4576b271125957cd5
JacquesFrazier <129914599+JacquesFrazier@users.noreply.github.com>
2023-09-12 14:37:47 -0400

    Update Flatten-and-sort-an-array.js

--- 7kyu/Flatten-and-sort-an-array.js
+++ 7kyu/Flatten-and-sort-an-array.js
@@ -33,3 +33,15 @@ function flattenAndSort(array) {
 
 //answer 4
 
+function flattenAndSort(array) {
+  var newArray = [];
+  for(var i =0;i<array.length;i++)
+  {
+    for(var j=0;j<array[i].length;j++)
+    {
+      newArray.push(array[i][j]);
+    }
+  }
+    
+  return newArray.sort((a,b) =>  a-b);
+}
