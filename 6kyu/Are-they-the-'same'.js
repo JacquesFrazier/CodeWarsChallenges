10030846b9050eaaab0b6c7a87be1a9bcb22a77b
JacquesFrazier <129914599+JacquesFrazier@users.noreply.github.com>
2023-09-19 09:17:27 -0400

    Update Are-they-the-'same'.js

--- 6kyu/Are-they-the-'same'.js
+++ 6kyu/Are-they-the-'same'.js
@@ -1,5 +1,4 @@
 //problem
-
 //Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears). "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
 
 //Examples
