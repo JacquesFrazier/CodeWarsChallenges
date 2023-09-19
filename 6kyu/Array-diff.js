993e1ebd9ec0bf8aa23effe7f4d3b883f2777cd1
JacquesFrazier <129914599+JacquesFrazier@users.noreply.github.com>
2023-09-19 09:16:38 -0400

    Update Array-diff.js

--- 6kyu/Array-diff.js
+++ 6kyu/Array-diff.js
@@ -1,5 +1,4 @@
 //problem
-
 //Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
 
 //It should remove all values from list a, which are present in list b keeping their order.
