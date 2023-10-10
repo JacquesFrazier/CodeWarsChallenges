2fecb1be41be8e33939df304c353ec0d7aac23b7
JacquesFrazier <129914599+JacquesFrazier@users.noreply.github.com>
2023-10-10 09:55:21 -0400

    Update Abbreviate-a-Two-Word-Name.js

--- 8kyu/Abbreviate-a-Two-Word-Name.js
+++ 8kyu/Abbreviate-a-Two-Word-Name.js
@@ -14,3 +14,33 @@
 function abbrevName(name){
     return name.split(' ').map((name) => name[0]).join('.').toUpperCase()
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//continue to pratice 
+
+//problem
+//Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+//The output should be two capital letters with a dot separating them.
+
+//It should look like this:
+
+//Sam Harris => S.H
+
+//patrick feeney => P.F
+
+
