cc90486165d487428e5f9049df72060d1b3c8e0a
JacquesFrazier <129914599+JacquesFrazier@users.noreply.github.com>
2023-10-18 11:15:39 -0400

    Update Are-You-Playing-Banjo.js

--- 8kyu/Are-You-Playing-Banjo.js
+++ 8kyu/Are-You-Playing-Banjo.js
@@ -31,3 +31,49 @@ function areYouPlayingBanjo(name) {
   function areYouPlayingBanjo(name) {
     return name + (name[0].toLowerCase() == 'r' ? ' plays' : ' does not play') + " banjo";
   }
+
+
+
+
+
+
+
+
+
+
+
+//continue pratice
+
+
+
+
+
+
+
+
+
+//problem 
+//Create a function which answers the question "Are you playing banjo?".
+//If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+//The function takes a name as its only argument, and returns one of the following strings:
+
+//name + " plays banjo" 
+//name + " does not play banjo"
+//Names given are always valid strings.
+
+
+//solution
+function banjoName(name){
+    return name[0] === 'r'.toLowerCase() ? return name + "plays bango" 
+        : return name + "does not play banjo";
+
+
+        
+}
+
+
+
+
+
+
