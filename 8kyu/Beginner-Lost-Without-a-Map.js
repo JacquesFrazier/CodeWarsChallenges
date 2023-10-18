8ec19a15d1b2779ec017018bc3fac3a330ac0c5e
JacquesFrazier <129914599+JacquesFrazier@users.noreply.github.com>
2023-10-18 11:36:58 -0400

    Update Beginner-Lost-Without-a-Map.js

--- 8kyu/Beginner-Lost-Without-a-Map.js
+++ 8kyu/Beginner-Lost-Without-a-Map.js
@@ -22,3 +22,40 @@ function maps(x){
     return x.map(n => n * 2);
   }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//continue to pratice 
+
+//problem
+
+//Given an array of integers, return a new array with each value doubled.
+
+//For example:
+
+[1, 2, 3] --> [2, 4, 6]
+
+
+//solution 
+
+function map(x){
+    
+}
+
+
+
+
+
+
