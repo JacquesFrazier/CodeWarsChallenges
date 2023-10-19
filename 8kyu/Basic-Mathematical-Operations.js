8676b97aaf9461172ef63d1891b27a9267eff524
JacquesFrazier <129914599+JacquesFrazier@users.noreply.github.com>
2023-10-19 12:50:14 -0400

    Update Basic-Mathematical-Operations.js

--- 8kyu/Basic-Mathematical-Operations.js
+++ 8kyu/Basic-Mathematical-Operations.js
@@ -69,4 +69,22 @@ Examples(Operator, value1, value2) --> output
 
 
 //solution
-function basicOp(operation, value1, value2) {}
+function basicOp(operation, value1, value2) {
+switch (operation){
+    case '+': 
+        return value1 + value2;
+    break;
+    case '-':
+        return value1 = value2;
+    break;
+    case '*':
+        return value1 * value2;
+    break;
+    case '/': 
+        return value1 / value2;
+    break;
+}    
+}
+
+
+
