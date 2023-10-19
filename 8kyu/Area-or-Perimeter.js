09ba59603b10e0f61aa3769dc6c7eb6ead2f55de
JacquesFrazier <129914599+JacquesFrazier@users.noreply.github.com>
2023-10-19 11:40:21 -0400

    Update Area-or-Perimeter.js

--- 8kyu/Area-or-Perimeter.js
+++ 8kyu/Area-or-Perimeter.js
@@ -58,7 +58,7 @@ const areaOrPerimeter = function(l , w) {
 
 //solution
 const areaOrPerimeter = function(l , w){
-    return l === w ? 2(l*w) : l * w;    
+    return l === w ? l * w : 2(l + w);    
 }
 
 
