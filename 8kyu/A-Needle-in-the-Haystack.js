4d0315ea8c607aeea1bf119eaabe674d44ea320f
JacquesFrazier <129914599+JacquesFrazier@users.noreply.github.com>
2023-10-29 12:14:43 -0400

    Update A-Needle-in-the-Haystack.js

--- 8kyu/A-Needle-in-the-Haystack.js
+++ 8kyu/A-Needle-in-the-Haystack.js
@@ -55,5 +55,5 @@ function findNeedle(haystack) {
 //solution -> good!
 
 function findNeedle(haystack){
-    return "found the needle at position " + haystack.indexOf("needle");
+    return "found the needle at position " + haystack.indexOf('needle'); 
 }
