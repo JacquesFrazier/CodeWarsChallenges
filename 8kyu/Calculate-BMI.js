2caa4db35c5dba9e2f81f7e82013b49623084de1
JacquesFrazier <129914599+JacquesFrazier@users.noreply.github.com>
2023-10-29 13:03:03 -0400

    Update Calculate-BMI.js

--- 8kyu/Calculate-BMI.js
+++ 8kyu/Calculate-BMI.js
@@ -23,3 +23,49 @@ function bmi(weight, height) {
       return "Obese"
     }
   }
+
+
+
+
+
+
+
+
+
+
+
+
+//continue to pratice 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//problem 
+//Write function bmi that calculates body mass index (bmi = weight / height2).
+
+//if bmi <= 18.5 return "Underweight"
+
+//if bmi <= 25.0 return "Normal"
+
+//if bmi <= 30.0 return "Overweight"
+
+//if bmi > 30 return "Obese"
+
+
+
+//solution 
+
+function bmi(weight, height) {
+let calculate = weight / (height * 2);
+    return calculate <= 18.5 ? "Underweight" : calculate <= 25.0 ? "Normal" : calculate <= 30.0 ? "Overweight" : calculate > 30 : "Obese";
+}
