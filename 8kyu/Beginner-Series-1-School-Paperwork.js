83b20cc4bb097e17d7b8678ab86def8eb8e7334e
JacquesFrazier <129914599+JacquesFrazier@users.noreply.github.com>
2023-10-29 12:55:46 -0400

    Update Beginner-Series-1-School-Paperwork.js

--- 8kyu/Beginner-Series-1-School-Paperwork.js
+++ 8kyu/Beginner-Series-1-School-Paperwork.js
@@ -23,3 +23,31 @@ function paperwork(n, m) {
       return 0;
     }
   }
+
+
+
+
+
+
+
+
+
+
+//continue to pratice 
+
+
+//problem
+//Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+//Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+
+//Example:
+//n= 5, m=5: 25
+//n=-5, m=5:  0
+
+
+//solution 
+
+function paperwork(n, m) {
+    return n === m ? n * m : n < 0 || m < 0 ? 0 : 'undefined';
+}
