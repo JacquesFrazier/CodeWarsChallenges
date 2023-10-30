605f56c5b729da6267cfab24690c02271ddcf930
JacquesFrazier <129914599+JacquesFrazier@users.noreply.github.com>
2023-10-30 13:28:59 -0400

    Update Calculate-average.js

--- 8kyu/Calculate-average.js
+++ 8kyu/Calculate-average.js
@@ -27,3 +27,30 @@ function find_average(array) {
 function find_average(arr) {
     return arr.length > 0? arr.reduce((a, b) => a + b) / arr.length : 0;
 }
+
+
+
+
+
+
+
+//continue to pratice 
+
+
+
+
+
+
+
+
+
+
+//question 
+
+//Write a function which calculates the average of the numbers in a given list.
+
+//Note: Empty arrays should return 0.
+
+
+
+//answer 
