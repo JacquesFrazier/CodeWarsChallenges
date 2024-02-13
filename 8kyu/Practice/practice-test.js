bf6ad01c501d6262222f14df7cd0eff2c767ec6c
Jacques Frazier <129914599+JacquesFrazier@users.noreply.github.com>
2024-02-13 13:58:17 -0500

    Update practice-test.js

--- 8kyu/Practice/practice-test.js
+++ 8kyu/Practice/practice-test.js
@@ -1,1 +1,43 @@
 //TEST YOUR MIGHT!!!!
+
+//1
+//question 
+//Can you find the needle in the haystack?
+
+//Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+//After your function finds the needle it should return a message (as a string) that says:
+
+//"found the needle at position " plus the index it found the needle, so:
+
+//Example(Input --> Output)
+
+//["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5" 
+
+
+//2
+
+
+//3
+
+
+//4
+
+
+//5
+
+
+//6
+
+
+//7
+
+
+//8
+
+
+//9
+
+
+//10
+
