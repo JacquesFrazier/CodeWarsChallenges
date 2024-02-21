d9ca60e0a4d452ba3bd5bed29865f76a354ab9fd
Jacques Frazier <129914599+JacquesFrazier@users.noreply.github.com>
2024-02-21 11:05:01 -0500

    Update practice-test.js

--- 8kyu/Practice/practice-test.js
+++ 8kyu/Practice/practice-test.js
@@ -112,6 +112,18 @@ function findMean(average){
 }
 
 //7
+//problem
+
+//Your task is to create a function that does four basic mathematical operations.
+
+//The function should take three arguments - operation(string/char), value1(number), value2(number).
+//The function should return result of numbers after applying the chosen operation.
+
+Examples(Operator, value1, value2) --> output
+('+', 4, 7) --> 11
+('-', 15, 18) --> -3
+('*', 5, 5) --> 25
+('/', 49, 7) --> 7
 
 
 //8
