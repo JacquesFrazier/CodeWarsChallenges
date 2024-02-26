d4be0f388e26f020a953f302336121b83a4c66fb
Jacques Frazier <129914599+JacquesFrazier@users.noreply.github.com>
2024-02-25 21:47:58 -0500

    Update practice-test.js

--- 8kyu/Practice/practice-test.js
+++ 8kyu/Practice/practice-test.js
@@ -266,7 +266,13 @@ Array's length > 2.
 
 
 //15
+//problem 
+//Write function bmi that calculates body mass index (bmi = weight / height2).
 
+//if bmi <= 18.5 return "Underweight"
+//if bmi <= 25.0 return "Normal"
+//if bmi <= 30.0 return "Overweight"
+//if bmi > 30 return "Obese"
 
 
 
