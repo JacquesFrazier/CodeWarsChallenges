7190175db9eebf15048b5a29141c7f76b72d34b1
Jacques Frazier <129914599+JacquesFrazier@users.noreply.github.com>
2024-02-25 21:44:40 -0500

    Update practice-test.js

--- 8kyu/Practice/practice-test.js
+++ 8kyu/Practice/practice-test.js
@@ -217,6 +217,16 @@ function time(h, s, m){
 }
 
 //11
+//The cockroach is one of the fastest insects. 
+//Write a function which takes its speed in km per hour 
+//and returns it in cm per second, rounded down to the integer (= floored).
+
+//For example:
+1.08 --> 30
+
+//Note! The input is a Real number (actual type is language dependent) and is >= 0. 
+//The result should be an Integer.
+
 
 //12
 
