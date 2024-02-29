ef8e5bd2580f34f6bf7873b43cf8be844b96e69e
Jacques Frazier <129914599+JacquesFrazier@users.noreply.github.com>
2024-02-29 11:18:08 -0500

    Update practice-test.js

--- 8kyu/Practice/practice-test.js
+++ 8kyu/Practice/practice-test.js
@@ -313,6 +313,12 @@ function bmi(weight, height){
 //solution 2
 
 //17
+//problem 
+//The first century spans from the year 1 up to 
+//and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+//Task
+//Given a year, return the century it is in.
 
 //18
 
