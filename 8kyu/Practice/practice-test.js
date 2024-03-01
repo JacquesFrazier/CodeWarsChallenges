bf25fb7a4de00c302a8899a399558b201a5fd6e6
Jacques Frazier <129914599+JacquesFrazier@users.noreply.github.com>
2024-03-01 11:37:45 -0500

    Update practice-test.js

--- 8kyu/Practice/practice-test.js
+++ 8kyu/Practice/practice-test.js
@@ -354,3 +354,11 @@ Examples
 
 
 //20
+//question
+// Write a function to split a string and convert it into an array of words.
+
+// Examples (Input ==> Output):
+// "Robin Singh" ==> ["Robin", "Singh"]
+
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
