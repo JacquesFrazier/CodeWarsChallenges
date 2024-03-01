3a7b5ec542934bffdf5c49dc843f71e20720ec8b
Jacques Frazier <129914599+JacquesFrazier@users.noreply.github.com>
2024-03-01 11:58:31 -0500

    Create Product-of-consecutive-Fib-numbers.js

---
+++ 5kyu/Product-of-consecutive-Fib-numbers.js
@@ -0,0 +1,84 @@
+//problem
+The Fibonacci numbers are the numbers in the following integer sequence (Fn):
+
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
+
+such as
+
+F(n) = F(n-1) + F(n-2) with F(0) = 0 and F(1) = 1.
+
+Given a number, say prod (for product), we search two Fibonacci numbers F(n) and F(n+1) verifying
+
+F(n) * F(n+1) = prod.
+
+Your function productFib takes an integer (prod) and returns an array:
+
+[F(n), F(n+1), true] or {F(n), F(n+1), 1} or (F(n), F(n+1), True)
+depending on the language if F(n) * F(n+1) = prod.
+
+If you don't find two consecutive F(n) verifying F(n) * F(n+1) = prodyou will return
+
+[F(n), F(n+1), false] or {F(n), F(n+1), 0} or (F(n), F(n+1), False)
+F(n) being the smallest one such as F(n) * F(n+1) > prod.
+
+Some Examples of Return:
+(depend on the language)
+
+productFib(714) # should return (21, 34, true), 
+                # since F(8) = 21, F(9) = 34 and 714 = 21 * 34
+
+productFib(800) # should return (34, 55, false), 
+                # since F(8) = 21, F(9) = 34, F(10) = 55 and 21 * 34 < 800 < 34 * 55
+-----
+productFib(714) # should return [21, 34, true], 
+productFib(800) # should return [34, 55, false], 
+-----
+productFib(714) # should return {21, 34, 1}, 
+productFib(800) # should return {34, 55, 0},        
+-----
+productFib(714) # should return {21, 34, true}, 
+productFib(800) # should return {34, 55, false}, 
+Note:
+You can see examples for your language in "Sample Tests".
+
+
+//answer 1
+function productFib(prod){
+  var n = 0;
+  var nPlus = 1;  
+  while(n*nPlus < prod) {
+    nPlus = n + nPlus;
+    n = nPlus - n;
+  }
+  return [n, nPlus, n*nPlus===prod];
+}
+
+//answer 2
+function productFib(prod){
+  let [a, b] = [0, 1];
+  while(a * b < prod) [a, b] = [b, a + b];
+  return [a, b, a * b === prod];
+}
+
+//answer 3
+function productFib(prod){
+  var a = 1
+  var b = 1;
+  while (a*b < prod) {
+    var next = a+b;
+    a = b;
+    b = next;
+  }
+  return [a, b, a*b===prod];
+}
+
+//answer 4
+function productFib(prod){
+  let a = 0;
+  let b = 1;
+  for (let i = 0; i < prod; i++) {
+    if (a * b > prod) return [a, b, false];
+    if (a * b === prod) return [a, b, true];
+    [a, b] = [b, a + b];
+  }
+}
