a5f968ab1f84045d1437a15fece19d2e012933c3
Jacques Frazier <129914599+JacquesFrazier@users.noreply.github.com>
2024-03-04 11:30:04 -0500

    Update practice-test.js

--- 8kyu/Practice/practice-test.js
+++ 8kyu/Practice/practice-test.js
@@ -367,9 +367,11 @@ function strToNum(str){
 // "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
 
 
+
 //21
 //question
 //Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
 
 
 //22
@@ -381,7 +383,18 @@ function strToNum(str){
 // 0 => [0]
 
 
-//23
 
+//23
+//problem
+//Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
+
+//When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
+
+//Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+
+//S is misinterpreted as 5
+//O is misinterpreted as 0
+//I is misinterpreted as 1
+//The test cases contain numbers only by mistake.
 
 
