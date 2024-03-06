d52b32c5887275ebd10c43c747f971a3336aca27
Jacques Frazier <129914599+JacquesFrazier@users.noreply.github.com>
2024-03-06 10:53:59 -0500

    Update practice-test.js

--- 8kyu/Practice/practice-test.js
+++ 8kyu/Practice/practice-test.js
@@ -453,4 +453,16 @@ function convert(n){
   return n.replace(/5/g, 'S').replace(/0/g, 'O').replace(/1/g, 'I')
 }
 
+//25
+//problem 
+//Create a function with two arguments that will return an array of the first n multiples of x.
+//Assume both the given number and the number of times to count will be positive numbers greater than 0.
+//Return the results as an array or list ( depending on language ).
 
+Examples
+countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+countBy(2,5) === [2,4,6,8,10]
+
+//solution 
+
+//26
