dc2e7967cc251d7768f2578d8873ce9ac6ae6605
Jacques Frazier <129914599+JacquesFrazier@users.noreply.github.com>
2024-03-07 08:32:37 -0500

    Update practice-test.js

--- 8kyu/Practice/practice-test.js
+++ 8kyu/Practice/practice-test.js
@@ -484,7 +484,8 @@ countBy(2,5) === [2,4,6,8,10]
 
 //27
 //question
-//Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+//Consider an array/list of sheep where some sheep may be missing from their place.
+//We need a function that counts the number of sheep present in the array (true means present).
 
 //For example,
 //[true,  true,  true,  false,
@@ -500,3 +501,26 @@ countBy(2,5) === [2,4,6,8,10]
 
 
 //28
+//problem 
+
+//Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. 
+//It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+
+//Ribonucleic acid, RNA, is the primary messenger molecule in cells. 
+//RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+//Create a function which translates a given DNA string into RNA.
+
+For example:
+
+"GCAT"  =>  "GCAU"
+//The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
+
+
+
+
+//29
+
+
+
+//30
