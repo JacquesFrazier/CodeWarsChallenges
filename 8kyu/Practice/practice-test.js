631cca8b20c81ef4d62b635b66f9fc95cf3ad097
Jacques Frazier <129914599+JacquesFrazier@users.noreply.github.com>
2024-03-07 08:29:44 -0500

    Update practice-test.js

--- 8kyu/Practice/practice-test.js
+++ 8kyu/Practice/practice-test.js
@@ -465,4 +465,21 @@ countBy(2,5) === [2,4,6,8,10]
 
 //solution 
 
+
+
 //26
+//problem 
+
+//Given an array of integers.
+
+//Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers.
+//0 is neither positive nor negative.
+
+//If the input is an empty array or is null, return an empty array.
+
+//Example
+//For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+
+
+//27
