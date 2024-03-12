f2e03d89325aeedc59a44e597c079fd89a8e4e25
Jacques Frazier <129914599+JacquesFrazier@users.noreply.github.com>
2024-03-12 14:26:30 -0400

    Update practice-test.js

--- 8kyu/Practice/practice-test.js
+++ 8kyu/Practice/practice-test.js
@@ -595,4 +595,18 @@ function cards(c){
 //If the sperm contains the X chromosome, return "Congratulations! You're going to have a daughter."; 
 //If the sperm contains the Y chromosome, return "Congratulations! You're going to have a son.";
 
-//solution 1
+//solution 
+
+
+//32
+//problem 
+//Trolls are attacking your comment section!
+
+//A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+//Your task is to write a function that takes a string and return a new string with all vowels removed.
+//For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+//Note: for this kata y isn't considered a vowel.
+
+//solution 
+
