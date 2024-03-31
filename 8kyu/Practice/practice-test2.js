f3598bfc95566002050821d3e11a7d9f2f1916a3
Jacques Frazier <129914599+JacquesFrazier@users.noreply.github.com>
2024-03-31 19:17:45 -0400

    Create practice-test2.js

---
+++ 8kyu/Practice/practice-test2.js
@@ -0,0 +1,29 @@
+//Lets Work!!!
+
+//31
+//problem 
+//The male gametes or sperm cells in humans and other mammals are heterogametic and contain one of two types of sex chromosomes. 
+//They are either X or Y. The female gametes or eggs however, contain only the X sex chromosome and are homogametic.
+
+//The sperm cell determines the sex of an individual in this case.
+//If a sperm cell containing an X chromosome fertilizes an egg, the resulting zygote will be XX or female. 
+//If the sperm cell contains a Y chromosome, then the resulting zygote will be XY or male.
+
+//Determine if the sex of the offspring will be male or female based on the X or Y chromosome present in the male's sperm.
+
+//If the sperm contains the X chromosome, return "Congratulations! You're going to have a daughter."; 
+//If the sperm contains the Y chromosome, return "Congratulations! You're going to have a son.";
+
+//solution 
+
+
+
+//32
+//problem 
+//Trolls are attacking your comment section!
+
+//A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+//Your task is to write a function that takes a string and return a new string with all vowels removed.
+//For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+//Note: for this kata y isn't considered a vowel.
